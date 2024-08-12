089564642b986f79a0b52705e096e46eb640ea71
estaciogoncalves <124896156+estaciogoncalves@users.noreply.github.com>
2024-08-12 06:17:33 -0400

    Update compl.l and token.h with the solutions

--- E3/src/token.h
+++ E3/src/token.h
@@ -2,14 +2,30 @@
 
 typedef enum {
         ID = 256, 
-	NUM,
-	ARRAY, // 258
-	BOOLEAN, // 259
-        KEY,
-        SYM,  
-        SYMC, 
+	      NUM,
+	      ARRAY, // 258
+	      BOOLEAN, // 259
+        KEY_CHAR, // 260
+        ELSE, // 261
+        FALSE, // 262
+        FOR, // 263
+        FUNCTION, // 264
+        IF, // 265
+        INTEGER, // 266
+        PRINT, // 267
+        RETURN, // 268
+        STRING, // 269
+        TRUE, // 270
+        VOID, // 271
+        WHILE, // 272
+        LEQ, // 273
+        GEQ, // 274
+        EQ, // 275
+        NEQ, // 276
+        AND, // 277
+        OR, // 278
+	      ERROR,
         CHAR,
-	STR,
-	ERROR
+        STR
 } token_t; 
 
